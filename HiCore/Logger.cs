c063f8b7065fdc11b2dbfca05071e3a85ad75aae
Matthias van Hogerhuis <matthiasvanhogerhuis017@gmail.com>
2026-01-19 12:37:37 +0100

    Add project files.

---
+++ HiCore/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiCore
+{
+    internal class Logger
+    {
+        public class logger() { }
+
+        public void info(string message)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("[info] ");
+            Console.ForegroundColor = originalColor;
+            Console.Write($"{message}\n");
+        }
+
+        public void Trace() { }
+        public void Warning() { }
+        public void Error() { }
+        public void Succes() { }
+
+    }
+}
---
+++ HiCore/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HiCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logOut = new Logger();
+
+            logOut.info("this is just info");
+        }
+    }
+}
