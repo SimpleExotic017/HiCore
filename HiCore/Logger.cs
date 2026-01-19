6a6c2c903320e6aed2f90757e80222e0d96d03b7
Matthias van Hogerhuis <matthiasvanhogerhuis017@gmail.com>
2026-01-19 12:53:22 +0100

    add logger contents

--- HiCore/Logger.cs
+++ HiCore/Logger.cs
@@ -10,7 +10,7 @@ namespace HiCore
     {
         public class logger() { }
 
-        public void info(string message)
+        public void Info(string message)
         {
             ConsoleColor originalColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Blue;
@@ -19,10 +19,34 @@ namespace HiCore
             Console.Write($"{message}\n");
         }
 
-        public void Trace() { }
-        public void Warning() { }
-        public void Error() { }
-        public void Succes() { }
+        public void Trace(string message) {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("[Trace] ");
+            Console.ForegroundColor = originalColor;
+            Console.Write($"{message}\n");
+        }
+        public void Warning(string message) {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("[Warning] ");
+            Console.ForegroundColor = originalColor;
+            Console.Write($"{message}\n");
+        }
+        public void Error(string message) {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("[Error] ");
+            Console.ForegroundColor = originalColor;
+            Console.Write($"{message}\n");
+        }
+        public void Succes(string message) {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("[Succes] ");
+            Console.ForegroundColor = originalColor;
+            Console.Write($"{message}\n");
+        }
 
     }
 }
--- HiCore/Program.cs
+++ HiCore/Program.cs
@@ -5,8 +5,13 @@
         static void Main(string[] args)
         {
             Logger logOut = new Logger();
+            string message = "this is just a message";
+            logOut.Info(message);
+            logOut.Trace(message);
+            logOut.Warning(message);
+            logOut.Error(message);
+            logOut.Succes(message);
 
-            logOut.info("this is just info");
         }
     }
 }
