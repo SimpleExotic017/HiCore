a24fb3341c16d2e55d2ffb2af675816beda74281
Matthias <matthiasvanhogerhuis017@gmail.com>
2026-02-07 22:25:39 +0100

    Add: add testing Environment

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using HiCore;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new InputFilter().QuestionToString("Do you have any questions for me?");
+        }
+    }
+}
