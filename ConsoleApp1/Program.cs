b147604d401dec5133fe1ee8208a28eafd610ec3
Matthias <matthiasvanhogerhuis017@gmail.com>
2026-02-10 10:57:21 +0100

    Update: test environment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,12 +7,12 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             InputFilter input = new InputFilter();
-            input.Man();
-            //string questions = input.QuestionToString("Do you have any questions for me?");
-            //int favoriteNumber = input.QuestionToInt("What's your favorite number");
-            //string favoriteColor = input.QuestionToString("What's your favorite Color");
-            //string favoriteCat = input.QuestionToString("What's your favorite Cat's name");
-            //Console.WriteLine($"you said \"{questions}\" When I asked you if you had questions\nyour favorite Number is \"{favoriteNumber}\"\nyour favorite Color is \"{favoriteColor}\"\nyour favorite Cat is \"{favoriteCat}\"\n");
+            //input.Man();
+            Logger logOut = new Logger();
+            logOut.Succes();
+            string[] names = { "DO NOT PRESS THIS BUTTON" };
+            Action[] methods = { () => logOut.Error("are you dumb?") };
+            new MethodPicker().Menu(names,methods);
         }
     }
 }
